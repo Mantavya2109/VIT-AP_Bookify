97fefbef83687321d78f96969831cfc5c1ee0b6c
Miheer Patel <miheer2109@gmail.com>
2026-01-13 01:13:56 +0530

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express"
+import { ENV } from "./config/env";
+import { clerkMiddleware } from '@clerk/express'
+import cors from 'cors';
+
+const app = express();
+const PORT = ENV.PORT;
+
+app.use(cors({origin: ENV.FRONTEND_URL}));
+app.use(clerkMiddleware());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.get("/", (req, res) => { 
+    res.json({
+        message: "Hello, World!",
+        endpoints: {
+            users: "/api/users",
+            products: "/api/products",
+            comments: "/api/comments",
+        }
+    }); 
+}); 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
