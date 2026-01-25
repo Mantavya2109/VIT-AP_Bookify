7c67b40d372735d1acd12f7fccf2c0b7b40db5a3
Miheer Patel <miheer2109@gmail.com>
2026-01-25 19:57:10 +0530

    Add initial frontend files including HTML, SVG, and image assets
    - Created index.html for the main entry point of the application.
    - Added vite.svg as a favicon for the application.
    - Included image.png in the dist directory for future use.

---
+++ backend/dist/routes/userRoutes.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const userController_1 = require("../controllers/userController");
+const express_2 = require("@clerk/express");
+const router = (0, express_1.Router)();
+router.post("/sync", (0, express_2.requireAuth)(), userController_1.syncUser);
+exports.default = router;
